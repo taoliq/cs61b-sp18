c6a4e8a5c5b5827941dd3ac32aba11d05cc6e2ec
logqtainia <taolq@outlook.com>
2019-06-11 16:32:17 +0900

    completed lab11 🤳

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +12,50 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(s);
+        marked[s] = true;
+        announce();
+
+        while (!queue.isEmpty()) {
+            int v = queue.poll();
+            for (int u : maze.adj(v)) {
+                if (!marked[u]) {
+                    distTo[u] = distTo[v] + 1;
+                    edgeTo[u] = v;
+                    marked[u] = true;
+                    queue.add(u);
+                    announce();
+                    if (u == t) {
+                        targetFound = true;
+                        return;
+                    }
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+         bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
+
 /**
  *  @author Josh Hug
  */
@@ -9,6 +12,7 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private boolean hasCycle = false;
 
     public MazeCycles(Maze m) {
         super(m);
@@ -16,9 +20,48 @@ public class MazeCycles extends MazeExplorer {
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        Stack<Integer> stk = new Stack<>();
+        stk.push(-1);
+        for (int i = 0; i < maze.V(); i++) {
+            if (!marked[i]) {
+                dfs(i, stk);
+            }
+        }
     }
 
     // Helper methods go here
+    private void dfs(int v, Stack<Integer> stk) {
+        if (hasCycle) {
+            return;
+        }
+        marked[v] = true;
+        int parent = stk.peek();
+        stk.push(v);
+        announce();
+
+        for (int u : maze.adj(v)) {
+            if (hasCycle) {
+                return;
+            }
+            if (!marked[u]) {
+                dfs(u, stk);
+            } else if (marked[u] && u != parent) {
+                int pre = stk.pop();
+                int cur;
+                edgeTo[u] = v;
+                while (pre != u) {
+                    cur = stk.pop();
+                    edgeTo[pre] = cur;
+                    pre = cur;
+                }
+                hasCycle = true;
+                announce();
+                return;
+            }
+        }
+        if (!stk.isEmpty()) {
+            stk.pop();
+        }
+    }
 }
 
