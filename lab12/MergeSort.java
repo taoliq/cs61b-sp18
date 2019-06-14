4b76b12827c7a784cafb94a42701afbc5d7e2b34
logqtainia <taolq@outlook.com>
2019-06-14 16:10:02 +0900

    sort is sweet 😎

--- lab12/MergeSort.java
+++ lab12/MergeSort.java
@@ -34,8 +34,13 @@ public class MergeSort {
     /** Returns a queue of queues that each contain one item from items. */
     private static <Item extends Comparable> Queue<Queue<Item>>
             makeSingleItemQueues(Queue<Item> items) {
-        // Your code here!
-        return null;
+        Queue<Queue<Item>> singleItemQueue = new Queue<>();
+        for (Item item : items) {
+            Queue<Item> queue = new Queue<>();
+            queue.enqueue(item);
+            singleItemQueue.enqueue(queue);
+        }
+        return singleItemQueue;
     }
 
     /**
@@ -53,14 +58,41 @@ public class MergeSort {
      */
     private static <Item extends Comparable> Queue<Item> mergeSortedQueues(
             Queue<Item> q1, Queue<Item> q2) {
-        // Your code here!
-        return null;
+        Queue<Item> sortedQueue = new Queue<>();
+        while (!q1.isEmpty() || !q2.isEmpty()) {
+            Item item = getMin(q1, q2);
+            sortedQueue.enqueue(item);
+        }
+        return sortedQueue;
     }
 
     /** Returns a Queue that contains the given items sorted from least to greatest. */
     public static <Item extends Comparable> Queue<Item> mergeSort(
             Queue<Item> items) {
-        // Your code here!
-        return items;
+        Queue<Queue<Item>> queues = makeSingleItemQueues(items);
+        while (queues.size() >= 2) {
+            Queue<Item> q1 = queues.dequeue();
+            Queue<Item> q2 = queues.dequeue();
+            queues.enqueue(mergeSortedQueues(q1, q2));
+        }
+        return queues.dequeue();
+    }
+
+    public static void main(String[] args) {
+        Queue<String> students = new Queue<String>();
+        students.enqueue("Vanessa");
+        students.enqueue("Alice");
+        students.enqueue("Ethan");
+        System.out.println(students);
+        System.out.println(mergeSort(students));
+
+        Queue<Character> chars = new Queue<>();
+        chars.enqueue('g');
+        chars.enqueue('b');
+        chars.enqueue('e');
+        chars.enqueue('u');
+        chars.enqueue('a');
+        System.out.println(chars);
+        System.out.println(mergeSort(chars));
     }
 }
--- lab12/QuickSort.java
+++ lab12/QuickSort.java
@@ -47,13 +47,49 @@ public class QuickSort {
     private static <Item extends Comparable> void partition(
             Queue<Item> unsorted, Item pivot,
             Queue<Item> less, Queue<Item> equal, Queue<Item> greater) {
-        // Your code here!
+        for (Item item : unsorted) {
+            int cmp = item.compareTo(pivot);
+            if (cmp < 0) {
+                less.enqueue(item);
+            } else if (cmp == 0) {
+                equal.enqueue(item);
+            } else {
+                greater.enqueue(item);
+            }
+        }
     }
 
     /** Returns a Queue that contains the given items sorted from least to greatest. */
     public static <Item extends Comparable> Queue<Item> quickSort(
             Queue<Item> items) {
-        // Your code here!
-        return items;
+        if (items.size() <= 1) {
+            return items;
+        }
+        Item pivot = getRandomItem(items);
+        Queue<Item> less = new Queue<>();
+        Queue<Item> equal = new Queue<>();
+        Queue<Item> greater = new Queue<>();
+        partition(items, pivot, less, equal, greater);
+        Queue<Item> sorted = catenate(quickSort(less), equal);
+        sorted = catenate(sorted, quickSort(greater));
+        return sorted;
+    }
+
+    public static void main(String[] args) {
+        Queue<String> students = new Queue<String>();
+        students.enqueue("Vanessa");
+        students.enqueue("Alice");
+        students.enqueue("Ethan");
+        System.out.println(students);
+        System.out.println(quickSort(students));
+
+        Queue<Character> chars = new Queue<>();
+        chars.enqueue('g');
+        chars.enqueue('b');
+        chars.enqueue('e');
+        chars.enqueue('u');
+        chars.enqueue('a');
+        System.out.println(chars);
+        System.out.println(quickSort(chars));
     }
 }
